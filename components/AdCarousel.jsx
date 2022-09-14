49987c835b61e469e8e1e06e7597f5e49a187e99
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-09-14 20:27:59 +0530

    added images for carousel

--- components/AdCarousel.jsx
+++ components/AdCarousel.jsx
@@ -1,18 +1,17 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
+import carousel1 from "../public/static/images/carousel1.jpg";
+import carousel2 from "../public/static/images/carousel2.jpg";
+import carousel3 from "../public/static/images/carousel3.jpg";
+import carousel4 from "../public/static/images/carousel4.jpg";
+import carousel5 from "../public/static/images/carousel5.jpg";
 
 import styles from "../styles/adCarousel.module.css";
 
 const AdCarousel = () => {
   const [index, setIndex] = useState(0);
 
-  const carousel_items = [
-    "https://pixabay.com/get/g51ebd4cdd063d4b6936639b0a975ba70322ed673a412d5e959cd91dac1e6921150be1d614fe1f0406e16fd545a29d777.jpg",
-    "https://pixabay.com/get/gf3afacc02f0b9f043300130f23250d4f17a978fdd6f6529e121cb5a08a1792552871d0d253d39b7857f30b6587f2df68.jpg",
-    "https://pixabay.com/get/g785814657d5a88dae97c1141b93c2ead6ffb48a6f04b280834b20bd39de9f871089f9124f29526a314984b61fc663090.jpg",
-    "https://pixabay.com/get/g233f20fed2775e8f4dc462294a1914d4ea2910392b9cbefb35716b61921722a6e6d0d18eb29bb9871ef71b5ffe4a91af.jpg",
-    "https://pixabay.com/get/g8309001dd01db0ecf9666b1e013d758c82975fbb05784cf59c158c393cdf4724a725b6160d5b07e5d84b7738d1d1fb61.jpg",
-  ];
+  const carousel_items = [carousel1,carousel2,carousel3,carousel4,carousel5];
   const length = carousel_items.length;
 
   const [myArr,setMyArr] = useState([...carousel_items]);
